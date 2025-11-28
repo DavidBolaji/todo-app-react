8b9fa462539389629bfceaa3ce10486a4a20dd91
davidbolaji <odavidbolaji14@gmail.com>
2025-11-28 15:52:45 +0100

    first app commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './components/TodoStyles.css'
+import TodoList from './components/TodoList';
+import TodoInput from './components/TodoInput';
+import TodoActions from './components/TodoActions';
+import { TodoProvider } from './context/TodoContext';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoProvider>
+      <div className="App">
+        <h1>Todo Application</h1>
+        <TodoInput />
+        <TodoActions />
+        <TodoList />
+      </div>
+    </TodoProvider>
   );
 }
 
