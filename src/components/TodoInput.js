9b68c7ec59e981edd1bc14b0e75f6a4e5707f989
DavidBolaji <70922550+DavidBolaji@users.noreply.github.com>
2025-12-05 19:18:31 +0100

    ReviveHub Migration: React to nextjs-app (batch 1/1: 18 files)

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,26 +1,46 @@
+'use client';
+
 import React from 'react';
 import useTodos from '../hooks/useTodos';
 
-const TodoInput = () => {
+const TodoInput: React.FC = () => {
   const { addTodo } = useTodos();
-  const [inputValue, setInputValue] = React.useState('');
+  const [inputValue, setInputValue] = React.useState<string>('');
 
-  const handleAddTodo = () => {
+  const handleAddTodo = (): void => {
     addTodo(inputValue);
     setInputValue('');
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.target.value);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter') {
+      handleAddTodo();
+    }
+  };
+
   return (
-    <div className="todo-input-container">
+    <div className="w-full my-2 mx-0">
       <input
         type="text"
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
         placeholder="Add a new todo"
-      />
-      <button onClick={handleAddTodo}>Add Todo</button>
-    </div>
-  );
+        aria-label="New todo input" />
+
+
+      <button
+        onClick={handleAddTodo}
+        aria-label="Add todo button">
+
+        Add Todo
+      </button>
+    </div>);
+
 };
 
 export default TodoInput;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,24 @@
+'use client';
+
 import React from 'react';
 import useTodos from '../hooks/useTodos';
 import TodoItem from './TodoItem';
 
-const TodoList = () => {
+const TodoList: React.FC = (): JSX.Element => {
   const { todos, toggleTodo, deleteTodo } = useTodos();
 
   return (
-    <ul>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onToggle={() => toggleTodo(todo.id)}
-          onDelete={() => deleteTodo(todo.id)}
-        />
-      ))}
-    </ul>
-  );
+    <ul role="list" aria-label="Todo list">
+      {todos.map((todo) =>
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        onToggle={() => toggleTodo(todo.id)}
+        onDelete={() => deleteTodo(todo.id)} />
+
+      )}
+    </ul>);
+
 };
 
 export default TodoList;
